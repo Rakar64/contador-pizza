de142d31b83f7dbc7d7bc86c45d7aa4fd07ba17e
henriqueramossaraiva@hotmail.com <henriqueramossaraiva@hotmail.com>
2024-11-23 18:40:10 -0300

    Update

--- script.js
+++ script.js
@@ -4,23 +4,40 @@ const amount = document.getElementById("amount");
 const change = document.getElementById("switch");
 const $html = document.querySelector('html');
 const easter = document.getElementById('foto');
+const note = document.getElementById('note');
+const saveBtn = document.getElementById('saveBtn');
 let counter = 0;
+
+window.onload = function(){
+    amount.innerText = localStorage.getItem('amountPizza')
+    note.innerText = localStorage.getItem('userNote')
+}
+
+function saveInfo(){
+    localStorage.setItem('amountPizza', parseInt(amount.innerText));
+    localStorage.setItem('userNote', note.value);
+}
 
 function pressUp(){  
     let el = parseInt(amount.innerText)
-    el+=1
+    el+=1 
     amount.innerText = el
-}
-function pressDown(){
-    let el = parseInt(amount.innerText)
-    el -= 1
-    if(el < 0){
-        amount.innerText = 0
-    }else{
-        amount.innerText = el
-    }
+    saveInfo()
 }
 
+function pressDown(){
+    let el = parseInt(amount.innerText)
+    el -= 1  
+    if(el < 0){
+        amount.innerText = 0
+    }else{saveInfo();
+        amount.innerText = el
+    }
+    saveInfo()
+}
+
+
+saveBtn.addEventListener("click", saveInfo);
 up.addEventListener("click", pressUp);
 down.addEventListener("click", pressDown);
 change.addEventListener('change', function(){
@@ -29,6 +46,6 @@ change.addEventListener('change', function(){
 easter.addEventListener('click', function(){
     counter += 1
     if(counter >= 10){
-        window.alert("Feliz Aniversário Andy")
+        window.alert("Era para ser um Easter Egg?")
     }
 })
